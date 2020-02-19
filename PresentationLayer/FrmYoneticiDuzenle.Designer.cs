5e2cc026fcc7306f8cf24036ace6ce351451e0b9
hamdidamar <hmdmr1@hotmail.com>
2020-02-19 17:38:26 +0300

    Yönetici Düzenle Form

---
+++ PresentationLayer/FrmYoneticiDuzenle.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace PresentationLayer
+{
+    partial class FrmYoneticiDuzenle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblYoneticiId = new System.Windows.Forms.Label();
+            this.txtYoneticiId = new System.Windows.Forms.TextBox();
+            this.txtKullaniciAd = new System.Windows.Forms.TextBox();
+            this.lblKullaniciAd = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.lblSifre = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.dgwYoneticiDuzenle = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwYoneticiDuzenle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblYoneticiId
+            // 
+            this.lblYoneticiId.AutoSize = true;
+            this.lblYoneticiId.Location = new System.Drawing.Point(48, 37);
+            this.lblYoneticiId.Name = "lblYoneticiId";
+            this.lblYoneticiId.Size = new System.Drawing.Size(65, 13);
+            this.lblYoneticiId.TabIndex = 0;
+            this.lblYoneticiId.Text = "Yönetici ID :";
+            // 
+            // txtYoneticiId
+            // 
+            this.txtYoneticiId.Location = new System.Drawing.Point(119, 34);
+            this.txtYoneticiId.Name = "txtYoneticiId";
+            this.txtYoneticiId.Size = new System.Drawing.Size(220, 20);
+            this.txtYoneticiId.TabIndex = 1;
+            // 
+            // txtKullaniciAd
+            // 
+            this.txtKullaniciAd.Location = new System.Drawing.Point(119, 76);
+            this.txtKullaniciAd.Name = "txtKullaniciAd";
+            this.txtKullaniciAd.Size = new System.Drawing.Size(220, 20);
+            this.txtKullaniciAd.TabIndex = 3;
+            // 
+            // lblKullaniciAd
+            // 
+            this.lblKullaniciAd.AutoSize = true;
+            this.lblKullaniciAd.Location = new System.Drawing.Point(45, 79);
+            this.lblKullaniciAd.Name = "lblKullaniciAd";
+            this.lblKullaniciAd.Size = new System.Drawing.Size(68, 13);
+            this.lblKullaniciAd.TabIndex = 2;
+            this.lblKullaniciAd.Text = "Kullanıcı Ad :";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(119, 119);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(220, 20);
+            this.txtSifre.TabIndex = 5;
+            // 
+            // lblSifre
+            // 
+            this.lblSifre.AutoSize = true;
+            this.lblSifre.Location = new System.Drawing.Point(79, 126);
+            this.lblSifre.Name = "lblSifre";
+            this.lblSifre.Size = new System.Drawing.Size(34, 13);
+            this.lblSifre.TabIndex = 4;
+            this.lblSifre.Text = "Şifre :";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(66, 164);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 6;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(182, 164);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(303, 164);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 8;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // dgwYoneticiDuzenle
+            // 
+            this.dgwYoneticiDuzenle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwYoneticiDuzenle.Location = new System.Drawing.Point(12, 214);
+            this.dgwYoneticiDuzenle.Name = "dgwYoneticiDuzenle";
+            this.dgwYoneticiDuzenle.Size = new System.Drawing.Size(388, 150);
+            this.dgwYoneticiDuzenle.TabIndex = 9;
+            // 
+            // FrmYoneticiDuzenle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(412, 373);
+            this.Controls.Add(this.dgwYoneticiDuzenle);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.lblSifre);
+            this.Controls.Add(this.txtKullaniciAd);
+            this.Controls.Add(this.lblKullaniciAd);
+            this.Controls.Add(this.txtYoneticiId);
+            this.Controls.Add(this.lblYoneticiId);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmYoneticiDuzenle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmYoneticiDuzenle";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwYoneticiDuzenle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblYoneticiId;
+        private System.Windows.Forms.TextBox txtYoneticiId;
+        private System.Windows.Forms.TextBox txtKullaniciAd;
+        private System.Windows.Forms.Label lblKullaniciAd;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label lblSifre;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.DataGridView dgwYoneticiDuzenle;
+    }
+}
